6d848b56dc885dcd58f5892592b36087ea2ebcad
mmuniz75 <muniz_marcelo@hotmail.com>
2017-03-08 11:53:51 -0300

    Adding Angular 2

---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'pm-app',
+    template: `
+        <h1>This is the future Ask Alien 2</h1>
+    `
+})
+export class AppComponent { }
