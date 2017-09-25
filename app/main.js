6939f34744852be497ec1bec660bd4a34a0b8a58
mmuniz75 <muniz_marcelo@hotmail.com>
2017-09-25 07:58:58 -0300

    set enable production

--- app/main.js
+++ app/main.js
@@ -3,5 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 // main entry point
 var platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
 var app_module_1 = require("./app.module");
+var core_1 = require("@angular/core");
+// Enable production mode unless running locally
+if (!/localhost/.test(document.location.host)) {
+    core_1.enableProdMode();
+}
 platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_module_1.AppModule);
 //# sourceMappingURL=main.js.map
